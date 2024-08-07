2d115c06f4af9739314c5ad58281cfc03f79cd48
PerSerMax <wundel.uber@yandex.ru>
2024-08-07 19:38:12 +0300

    commiiiiiiiiiiit

--- model.py
+++ model.py
@@ -5,10 +5,12 @@ import tensorflow as tf
 from tensorflow.keras.callbacks import ModelCheckpoint
 
 # КОНСТАНТЫ
-rev_len = 300
+rev_len = 2000
 model_path = "model.keras"
-batch_size = 256
+batch_size = 64
 epochs = 100000
+learning_rate = 0.05
+patience = 100
 
 def preprocess_text(text, rev_len):
     text_bytes = text.encode('utf-8')
@@ -23,15 +25,19 @@ else:
     # СОЗДАНИЕ МОДЕЛИ
     model = tf.keras.models.Sequential([
         tf.keras.layers.Dense(rev_len, activation='relu'),
+        tf.keras.layers.Dropout(0.5), 
         tf.keras.layers.Dense(rev_len // 2, activation="relu"),
+        tf.keras.layers.Dropout(0.5),
         tf.keras.layers.Dense(rev_len // 4, activation="relu"),
+        tf.keras.layers.Dropout(0.5),
         tf.keras.layers.Dense(rev_len // 8, activation="relu"),
+        tf.keras.layers.Dropout(0.5),
         tf.keras.layers.Dense(rev_len // 16, activation="relu"),
         tf.keras.layers.Dense(1, activation='sigmoid')
     ])
 
     model.compile(
-        optimizer=tf.keras.optimizers.Adam(learning_rate=0.05),
+        optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
         loss=tf.keras.losses.BinaryCrossentropy(),
         metrics=['accuracy']
     )
@@ -66,8 +72,7 @@ y_train = []
 for k in range(align_num):
     x_train.append(preprocess_text(good_reviews[k], rev_len))
     y_train.append(1)
-    x_train.append(preprocess_text(neutral_reviews[k], rev_len))
-    y_train.append(0.5)
+    # y_train.append(0.5) # Убираем нейтральные отзывы
     x_train.append(preprocess_text(bad_reviews[k], rev_len))
     y_train.append(0)
 
@@ -81,19 +86,19 @@ print(f"y_train shape: {y_train.shape}")
 # CALLBACKS
 early_stopping_val_loss = tf.keras.callbacks.EarlyStopping(
     monitor='val_loss',
-    patience=3000,
+    patience=patience,
     restore_best_weights=True
 )
 
 early_stopping_loss = tf.keras.callbacks.EarlyStopping(
     monitor='loss',
-    patience=3000,
+    patience=patience,
     restore_best_weights=True
 )
 
 checkpoint_callback = ModelCheckpoint(
     filepath='model_epoch_{epoch:02d}.keras',
-    save_freq=1000 * (x_train.shape[0] // batch_size),  # Сохраняет модель каждые 30 эпох
+    save_freq=800 * (x_train.shape[0] // batch_size),  # Сохраняет модель каждые 30 эпох
     save_best_only=False
 )
 
